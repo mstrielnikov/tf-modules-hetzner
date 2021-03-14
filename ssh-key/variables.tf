4b22a1e8017b73c3f8179425d089c9c3285ea303
mstrielnikov <makstr@ukr.net>
2021-03-14 12:09:55 +0200

    Added ssh key module

---
+++ ssh-key/variables.tf
@@ -0,0 +1,14 @@
+variable "ssh_key_name" {
+    type    = string
+    default = ""
+}
+
+variable "ssh_key_value" {
+    type    = string
+    default = ""
+}
+
+variable "ssh_key_filepath" {
+    type    = string
+    default = ""
+}
